9d745ec047b69c70d78c964f7e72fd355412dfaf
zukiin <zukinkalitshana@icloud.com>
2023-04-23 22:04:55 +0200

    Add _putchar and conversion 's' and 'c' functions

---
+++ 0-functions.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _printf_s - prints a string
+ * @arg: string
+ *
+ * Return: number of printed characters
+ */
+
+int _printf_s(void *arg)
+{
+	char *str = *(char **)arg;
+	int i = 0;
+
+	if (str == NULL)
+		return (-1);
+
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * _printf_c - prints a character
+ * @arg: a character to print.
+ * 
+ * Return: number of character printed
+ */
+
+int _printf_c(void *arg)
+{
+	char c = *(char *)arg;
+
+	_putchar(c);
+	return (1);
+}
