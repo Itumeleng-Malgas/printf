0f2c641ef112e7717dfb8538a05be4d1aebc19da
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-04-24 10:08:48 +0200

    Modified _printf_s, to print (null) if str to print is NULL

--- 0-functions.c
+++ 0-functions.c
@@ -13,7 +13,7 @@ int _printf_s(void *arg)
 	int i = 0;
 
 	if (!str)
-		return (-1);
+		str = "(null)";
 
 	while (*str != '\0')
 	{
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <limits.h>
 #include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -9,12 +10,15 @@
  */
 int main(void)
 {
+	char *str = NULL;
     _printf("Let's try to printf a simple sentence.\n");
 
     _printf("Character:[%c]\n", 'H');
     _printf("String:[%s]\n", "I am a string !");
     _printf("Percent:[%%]\n");
     _printf("Unknown:[%r]\n");
+	_printf("%%%%");
 
+	_printf("%s\n", str);
     return (0);
 }
